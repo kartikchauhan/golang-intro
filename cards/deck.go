3330e6c450c3c7c8333cb4355c98f5a24718cadf
kartikchauhan <chauhan.kartik25@gmail.com>
2022-10-16 01:45:07 +0530

    add comments

--- cards/deck.go
+++ cards/deck.go
@@ -4,7 +4,8 @@ import "fmt"
 
 type deck []string
 
-func (d deck) print() {
+// Golang's standard suggests to use the first letter of type as the variable name 
+func (d deck) print() { // print is a receiver which receives type deck
 
 	for index, card := range d {
 		fmt.Println(index, card)
