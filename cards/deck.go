7b7ef3a27a8f30e3bea7fc8fdefcedb76fc2b0cb
kartikchauhan <chauhan.kartik25@gmail.com>
2022-10-16 01:43:07 +0530

    add type deck

--- cards/deck.go
+++ cards/deck.go
@@ -1,3 +1,12 @@
 package main
 
+import "fmt"
+
 type deck []string
+
+func (d deck) print() {
+
+	for index, card := range d {
+		fmt.Println(index, card)
+	}
+}
--- cards/main.go
+++ cards/main.go
@@ -1,14 +1,10 @@
 package main
 
-import "fmt"
-
 var decksize int
 
 func main() {
 	cards := deck{"Ace of Spades", "Eight of Heart"}
 	cards = append(cards, "Three of Club")
 
-	for index, card := range cards {
-		fmt.Println(index, card)
-	}
+	cards.print()
 }
