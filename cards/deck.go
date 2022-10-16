e8f884e0858575f99dcdb4da724e0765894c154b
kartikchauhan <chauhan.kartik25@gmail.com>
2022-10-16 16:38:07 +0530

    add function newDeck

--- cards/deck.go
+++ cards/deck.go
@@ -4,7 +4,23 @@ import "fmt"
 
 type deck []string
 
-// Golang's standard suggests to use the first letter of type as the variable name 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Clubs", "Diamonds"}
+
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, number := range cardValues {
+		for _, suit := range cardSuits {
+			cards = append(cards, number+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+// Golang's standard suggests to use the first letter of type as the variable name
 func (d deck) print() { // print is a receiver which receives type deck
 
 	for index, card := range d {
--- cards/main.go
+++ cards/main.go
@@ -3,8 +3,9 @@ package main
 var decksize int
 
 func main() {
-	cards := deck{"Ace of Spades", "Eight of Heart"}
-	cards = append(cards, "Three of Club")
+	// cards := deck{"Ace of Spades", "Eight of Heart"}
+	// cards = append(cards, "Three of Club")
 
+	cards := newDeck()
 	cards.print()
 }
